2107f38479b5d811a171097fb729720fc84435d8
Karen Barrientos <karenp.bmedina@unitec.edu>
2019-02-18 21:31:08 -0600

    5

---
+++ src/data.js
@@ -0,0 +1,113 @@
+
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Boxer",
+    img: "img/1.jpg",
+    price: 3000,
+    company: "SuperPatitas",
+    info:
+      "Canino",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Boxer",
+    img: "img/2.jpg",
+    price: 3000,
+    company: "SuperPatitas",
+    info:
+      "Canino",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Chow Chow",
+    img: "img/11.jpg",
+    price: 2500,
+    company: "SuperPatitas2",
+    info:
+      "Canino",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Chow Chow",
+    img: "img/22.jpg",
+    price: 2500,
+    company: "SuperPatitas2",
+    info:
+      "Canino",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Chow Chow",
+    img: "img/33.jpg",
+    price: 2500,
+    company: "SuperPatitas2",
+    info:
+      "Canino",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Pitbull",
+    img: "img/111.jpg",
+    price: 4000,
+    company: "SuperPatitas3",
+    info:
+      "Canino",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Pitbull",
+    img: "img/222.jpg",
+    price: 4000,
+    company: "Canino",
+    info:
+      "Canino",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Pitbull",
+    img: "img/333.jpg",
+    price: 4000,
+    company: "SuperPatitas3",
+    info:
+      "Canino",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Boxer",
+  img: "img/1.jpg",
+  price: 3000,
+  company: "SuperPAtitas",
+  info:
+    "Canino",
+  inCart: false,
+  count: 0,
+  total: 0
+};
